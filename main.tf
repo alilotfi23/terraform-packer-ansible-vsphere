d4495f3da7126b6801eee380f263e9f17e7919ea
Ali Lotfi <91953142+alilotfi23@users.noreply.github.com>
2024-03-02 14:04:15 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,40 @@
+# Import the module
 module "my_vm_module" {
   source = "./my_terraform_module"
 
   # Set your variables here
-  vsphere_user        = "your_vsphere_user"
-  vsphere_password    = "your_vsphere_password"
-  vsphere_ip_address  = "your_vcenter_server_ip"
-  vsphere_datacenter  = "your_datacenter_name"
-  vsphere_cluster     = "your_cluster_name"
-  datastore           = "your_datastore_name"
-  vm_cpu              = 2
-  vm_ram              = 4096
-  vm_name             = "your_vm_name"
-  vm_size             = 50
+  vsphere_user           = "your_vsphere_user"
+  vsphere_password       = "your_vsphere_password"
+  vsphere_ip_address     = "your_vcenter_server_ip"
+  vsphere_datacenter     = "your_datacenter_name"
+  vsphere_cluster        = "your_cluster_name"
+  datastore              = "your_datastore_name"
+  vm_cpu                 = 2
+  vm_ram                 = 4096
+  vm_name                = "your_vm_name"
+  vm_size                = 50
   vsphere_unverified_ssl = true  # or false based on your needs
-  vm_network          = "your_vm_network"
-  cluster_name        = "your_cluster_name"
+  vm_network             = "your_vm_network"
+  cluster_name           = "your_cluster_name"  # Add this line
+}
+
+# Optionally, you can use the outputs from the module directly
+output "vm_ip_address" {
+  description = "The IP address of the provisioned virtual machine"
+  value       = module.my_vm_module.vm_ip_address
+}
+
+output "vm_name" {
+  description = "The name of the provisioned virtual machine"
+  value       = module.my_vm_module.vm_name
+}
+
+output "datastore_id" {
+  description = "The ID of the selected datastore"
+  value       = module.my_vm_module.datastore_id
+}
+
+output "cluster_name" {
+  description = "The name of the selected compute cluster"
+  value       = module.my_vm_module.cluster_name
 }
