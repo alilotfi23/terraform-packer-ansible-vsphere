a26d82d5f200fec1866c06838ce60f3216e56a17
Ali Lotfi <91953142+alilotfi23@users.noreply.github.com>
2024-03-02 13:57:11 +0330

    Update variable.tf

--- my_terraform_module/variable.tf
+++ my_terraform_module/variable.tf
@@ -10,7 +10,7 @@ variable "vsphere_password" {
 
 variable "vsphere_ip_address" {
   type        = string
-  description = "vCenter server  IP"
+  description = "vCenter server IP"
 }
 
 variable "vsphere_unverified_ssl" {
@@ -22,16 +22,19 @@ variable "vsphere_datacenter" {
   type        = string
   description = "vSphere datacenter"
 }
+
 variable "vsphere_cluster" {
-   type       = string
+  type        = string
   description = "vSphere cluster"
 }
+
 variable "datastore" {
   type        = string
   description = "vSphere datastore"
 }
+
 variable "vm_cpu" {
-   type       = number
+  type        = number
   description = "Number of vCPU for virtual machines"
 }
 
@@ -39,11 +42,23 @@ variable "vm_ram" {
   type        = number
   description = "Amount of Memory for virtual machines"
 }
+
 variable "vm_name" {
   type        = string
-  description = "The name of the  virtual machines"
+  description = "The name of the virtual machines"
 }
+
 variable "vm_size" {
   type        = number
   description = "The disk size of the virtual machine"
+}
+
+variable "vm_network" {
+  type        = string
+  description = "Name of the VM network"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the vSphere cluster"
 }
