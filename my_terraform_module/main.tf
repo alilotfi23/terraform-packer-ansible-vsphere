068acd76f0d409b9b9a630955f1888318f96c9c4
Ali Lotfi <91953142+alilotfi23@users.noreply.github.com>
2024-03-02 14:32:05 +0330

    Update main.tf

--- my_terraform_module/main.tf
+++ my_terraform_module/main.tf
@@ -12,6 +12,11 @@ data "vsphere_datastore" "datastore" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
+data "vsphere_datastore" "datacenter" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
 data "vsphere_compute_cluster" "cluster" {
   name          = var.cluster_name
   datacenter_id = data.vsphere_datacenter.datacenter.id
