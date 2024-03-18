3425132f58d60c7a4000c7f48c5c30757b44f2a3
Ali Lotfi <91953142+alilotfi23@users.noreply.github.com>
2024-03-18 23:47:34 +0330

    Add count with condition

--- main.tf
+++ main.tf
@@ -17,20 +17,21 @@ module "my_vm_module" {
   vm_network             = "your_vm_network"
   cluster_name           = "your_cluster_name"  
   guest_id               = "your_guest_id"      
-  path                   = "your_path"          
+  path                   = "your_path" 
+  count                  = var.create_vm ? 1 : 0 # Add count with condition
 }
 
 output "vm_name" {
   description = "The name of the provisioned virtual machine"
-  value       = module.my_vm_module.vm_name
+  value       = module.my_vm_module[*].vm_name
 }
 
 output "datastore_id" {
   description = "The ID of the selected datastore"
-  value       = module.my_vm_module.datastore_id
+  value       = module.my_vm_module[*].datastore_id
 }
 
 output "cluster_name" {
   description = "The name of the selected compute cluster"
-  value       = module.my_vm_module.cluster_name
+  value       = module.my_vm_module[*].cluster_name
 }
